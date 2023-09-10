7112919d4100a48ab5cefaaf00d7e606b1e3a988
Melson Heo <melsonheo646@gmail.com>
2023-09-09 21:15:20 -0400

    test

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -17,21 +17,21 @@ function App() {
         <table>
           <thead>
             <tr>
-              <th>Header 1</th>
-              <th>Header 2</th>
-              <th>Header 3</th>
+              <th></th>
+              <th>Favorite Color</th>
+              <th>Favorite Food</th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td>Data 1</td>
-              <td>Data 2</td>
-              <td>Data 3</td>
+              <td>John</td>
+              <td>Red</td>
+              <td>Pizza</td>
             </tr>
             <tr>
-              <td>Data 4</td>
-              <td>Data 5</td>
-              <td>Data 6</td>
+              <td>Blake</td>
+              <td>Blue</td>
+              <td>Sushi</td>
             </tr>
           </tbody>
         </table>
